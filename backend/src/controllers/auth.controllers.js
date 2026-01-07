3a33f9d574b9bd9f6e02847f62b38432d42aacdf
Prakhar0136 <prakharps010306@gmail.com>
2026-01-07 12:50:56 +0530

    signup section code refactored

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -32,6 +32,10 @@ try{
     if(newUser){
         generateToken(newUser._id,res)
         await newUser.save();
+
+        const savedUser = await newUser.save()
+        generateToken(savedUser._id,res)
+
         res.status(201).json({
             _id:newUser._id,
             fullName:newUser.fullName,
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.MONGOURI);
+        const {MONGOURI} = process.env;
+        if(!MONGOURI) throw new Error("MONGOURI IS NOT SET");
+        
+        const conn = await mongoose.connect(MONGOURI);
         console.log("mongodb conncected",conn.connection.host)
     }
     catch(error){
