5574843ff75f523721c680ddbbcf2b0d1000ea0b
Prakhar0136 <prakharps010306@gmail.com>
2026-01-10 21:25:03 +0530

    fixed something

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 
 const app = express()
-
 
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); 
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
